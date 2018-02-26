dfd430258f8fe8ac7f94caa68e791b6167d61c3b
Aleksandr Linkov <aleksandr_linkov@epam.com>
2018-02-26 15:08:52 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  images;
+
+  randomNumber(limit) {
+    return Math.floor(Math.random() * limit) + 1;
+  }
+
+  ngOnInit() {
+    this.images = Array.from({ length: 50 }, () => [this.randomNumber(4), this.randomNumber(4), this.randomNumber(15)])
+      .concat([[1, 1, this.randomNumber(15)]])
+    ;
+  }
+
 }
