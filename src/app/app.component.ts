e609ccad115a61cd5c60c923289484e65cc600b5
Aleksandr Linkov <aleksandr_linkov@epam.com>
2018-02-26 16:17:41 +0400

    control panel added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
   images;
+  dense_flow = false;
+  grid_gap = 20;
+  image_size = 100;
 
   randomNumber(limit) {
     return Math.floor(Math.random() * limit) + 1;
