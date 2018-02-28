4c04cb0200136ff9576fb0b4d1292341580e3365
Aleksandr Linkov <aleksandr_linkov@epam.com>
2018-02-28 12:49:29 +0400

    positioning added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import {ImageGalleryComponent} from './image-gallery/image-gallery.component';
 import {CompatibilityComponent} from './compatibility/compatibility.component';
 import {GridConstructorComponent} from './grid-constructor/grid-constructor.component';
 import {ZoningAndPlacingComponent} from './zoning-and-placing/zoning-and-placing.component';
+import { PositioningComponent } from './positioning/positioning.component';
+import { FlexGridComponent } from './flex-grid/flex-grid.component';
 
 const routes: Routes = [
   {path: 'compatibility', component: CompatibilityComponent},
   {path: 'constructor', component: GridConstructorComponent},
   {path: 'gallery', component: ImageGalleryComponent},
   {path: 'area', component: ZoningAndPlacingComponent},
+  {path: 'flex-grid', component: FlexGridComponent},
+  {path: 'positioning', component: PositioningComponent},
   {path: '**', redirectTo: 'compatibility'}
 ];
 
@@ -25,7 +29,9 @@ const routes: Routes = [
     ImageGalleryComponent,
     CompatibilityComponent,
     GridConstructorComponent,
-    ZoningAndPlacingComponent
+    ZoningAndPlacingComponent,
+    PositioningComponent,
+    FlexGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/positioning/positioning.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positioning',
+  templateUrl: './positioning.component.html',
+  styleUrls: ['./positioning.component.css']
+})
+export class PositioningComponent implements OnInit {
+  items = ['p-item-1', 'p-item-2', 'p-item-3', 'p-item-4', 'LOOK AT ME!!!', 'p-item-6', 'p-item-7', 'p-item-8', 'p-item-9', 'p-item-10'];
+  containerDefault = {
+    'height': '500px',
+    'display': 'grid',
+    'grid-gap': '20px',
+    'grid-template-columns': 'repeat(5, 130px)',
+    'border': '4px solid #ffffffaa'
+  };
+  containerStyle = {
+    'height': '500px',
+    'display': 'grid',
+    'grid-gap': '20px',
+    'grid-template-columns': 'repeat(5, 130px)',
+    'border': '4px solid #ffffffaa'
+  };
+  itemDefault = {'display': 'grid'};
+  itemStyle = {'display': 'grid'};
+  container_prefix: string;
+  container_modifier: string;
+  container_val: string;
+  item_prefix: string;
+  item_modifier: string;
+  item_val: string;
+  constructor() { }
+
+  changeContainerStyle() {
+    if (this.container_prefix && this.container_modifier && this.container_val) {
+      this.containerStyle = Object.assign({}, this.containerDefault);
+      const key = this.container_prefix + this.container_modifier;
+      this.containerStyle[key] = this.container_val;
+    }
+  }
+
+  changeItemStyle() {
+    if (this.item_prefix && this.item_modifier && this.item_val) {
+      this.itemStyle = Object.assign({}, this.itemDefault);
+      const key = this.item_prefix + this.item_modifier;
+      this.itemStyle[key] = this.item_val;
+    }
+  }
+
+  saveContainerRules() {
+    this.containerDefault = Object.assign({}, this.containerStyle);
+  }
+
+  saveItemRules() {
+    this.itemDefault = Object.assign({}, this.itemStyle);
+  }
+
+  ngOnInit() {
+  }
+
+}
